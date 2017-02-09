be854e18dbfb469371a2405cb7a1f9bd85ff5ef1
Sceleris Artifex <linux.usaf@live.com>
2017-02-09 13:08:33 -0600

    foreign agent base complete

---
+++ mobile/foreign_agent.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python
+
+# Brian Scott / Justin Largent
+# COSC 4653 - Advanced Networks
+# Assignment 3 - Mobile IP Simulation
+# foreign_agent.py
+
+# This bla bla bla
+
+# Developed on Kali Linux 2
+# Status: in development
+
+import sys
+from socket import *
+from signal import *
+
+s=socket(AF_INET,SOCK_DGRAM)
+s.bind(('',8000))
+
+def sh(signal,frame):
+  print('killing mobile agent')
+  s.close()
+  sys.exit(0)
+signal(SIGINT, sh)
+print 'listening\n'
+
+while 1:
+  d,a=s.recvfrom(1024)
+  f=d.split()
+  print f
